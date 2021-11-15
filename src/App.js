d8456c65fa19452d8cd7ccb0a604ac7f9efb7054
JckD <c16335383@mydit.ie>
2021-11-15 18:35:28 +0000

    added nav + banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import wolfgangLogo from './images/wolfgang_logo.svg'
+import contentImage from './images/content_image.jpg'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+       
+          
+          <div className="nav">
+            <a href="https://www.wolfgangdigital.com/">
+            <img className="logo" alt="wolfgang digital logo" src={wolfgangLogo}/>
+
+            </a>
+            <ul>
+              <li>
+                <a href="https://www.wolfgangdigital.com/our-work/">our work</a>
+              </li>
+              <li>
+                <a href="https://www.wolfgangdigital.com/about-us/">about us</a>
+              </li>
+              <li>
+                <a href="https://www.wolfgangdigital.com/careers/">careers</a>
+              </li>
+              <li>
+                <a href="https://www.wolfgangdigital.com/blog/">blog</a>
+              </li>
+              <li>
+                <a href="https://www.wolfgangdigital.com/awards/">awards</a>
+              </li>
+              <li>
+                <a href="https://www.wolfgangdigital.com/contact/">contact</a>
+              </li>
+            </ul>
+          </div>
+
+       
+           
       </header>
+      <body>
+          <div className="banner">
+            <h1>The Awards Hall</h1>
+            {/* <div className="white-line">test</div> */}
+          </div>
+          <div className="about">
+            <div className="text-container">
+              <p>
+                Back in 2011, when Wolfgang was only two people we submitted some case studies to the Digital Media Awards, optimistically entering the "Best Agency" category. At that time nobody knew who we were and our clients were not of the "catwalk" variety. So we certainly didn't expect to win, we optimistically hoped to get one of those cool "finalist" badges for our email signatures. Lo and behold, the judges loved our case studies, scored them strongly and we took the Gold Medal* home.</p>
+              <p>
+                This success imbued us with a wolfish hunger to execute outstanding campaigns and win further domestic & international awards.
+              </p>
+            </div>
+            <div className="img-container">
+              <img src={contentImage} alt="wolfgang awards ceremony" height="260" width="460"/>
+            </div>
+          </div>
+          <div className="awards">
+    
+          </div>
+      </body>
     </div>
   );
 }
